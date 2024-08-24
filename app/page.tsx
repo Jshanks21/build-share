09ac4880a108ffb287a2af3927a04efdc203716a
Luis Egea <alphasearchseo@gmail.com>
2024-08-24 12:58:27 +0100

    feat: add responsive navbar w/ theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Login from '@/components/Login';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Build & Share</h1>
-      <Login />
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Build & Share</h1>
+        <Login />
+      </main>
+    </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,135 @@
+'use client';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isdark, setIsdark] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const storedIsdark = localStorage.getItem('isdark');
+    setIsdark(storedIsdark === 'true');
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isdark', JSON.stringify(isdark));
+  }, [isdark]);
+
+  return (
+    <nav className="bg-base-100 shadow-lg">
+      <div className="container mx-auto px-4 py-2 flex justify-between">
+        <Link href="/">
+          Build &amp; Share
+        </Link>
+        <div className="hidden md:flex items-center space-x-4">
+          <Link href="/features">
+            Features
+          </Link>
+          <Link href="/pricing">
+            Pricing
+          </Link>
+          <Link href="/blog">
+            Blog
+          </Link>
+          <Link href="/signin">
+            Sign In
+          </Link>
+          <label className="swap swap-rotate">
+            <input type="checkbox" className="theme-controller" value="light" onChange={() => setIsdark(!isdark)} checked={!!isdark} />
+
+            {/* sun icon */}
+            <svg
+              className="swap-off h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24">
+              <path
+                d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+            </svg>
+
+            {/* moon icon */}
+            <svg
+              className="swap-on h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24">
+              <path
+                d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            </svg>
+          </label>
+        </div>
+        
+        {/* mobile menu */}
+        <div className="md:hidden flex gap-2">
+          <label className="swap swap-rotate">
+            <input type="checkbox" className="theme-controller" value="light" onChange={() => setIsdark(!isdark)} checked={!!isdark} />
+
+            {/* sun icon */}
+            <svg
+              className="swap-off h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24">
+              <path
+                d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+            </svg>
+
+            {/* moon icon */}
+            <svg
+              className="swap-on h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24">
+              <path
+                d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            </svg>
+          </label>
+          <button
+            className={`flex items-center px-3 py-2 border rounded hover:text-white hover:border-white ${!isdark ? 'text-teal-200 border-teal-400' : 'text-black border-black'}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              className="fill-current h-4 w-4"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0V15z" />
+            </svg>
+          </button>
+          {isOpen && (
+            <div className="absolute top-0 right-0 w-full md:hidden">
+              <div className="bg-base-100 p-4">
+                <button
+                  className={`absolute top-2 right-4 px-3 py-2 border rounded hover:text-white hover:border-white ${!isdark ? 'text-teal-200 border-teal-400' : 'text-black border-black'}`}
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  <svg
+                    className="fill-current h-4 w-4"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Close</title>
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+                <Link href="/features" className="block text-lg px-4 py-2">
+                  Features
+                </Link>
+                <Link href="/pricing" className="block text-lg px-4 py-2">
+                  Pricing
+                </Link>
+                <Link href="/blog" className="block text-lg px-4 py-2">
+                  Blog
+                </Link>
+                <Link href="/signin" className="btn btn-primary block w-full text-lg px-4 py-2">
+                  Sign In
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
